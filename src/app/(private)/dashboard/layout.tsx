1c338da611c0742dbb8dd368e19cf269b63fc398
Aleksandr Barmin <abarmin@outlook.com>
2024-10-28 17:45:24 +0000

    Back to AuthJS as it is the simplest option ever

--- src/app/(private)/dashboard/layout.tsx
+++ src/app/(private)/dashboard/layout.tsx
@@ -1,7 +1,9 @@
 import "@/app/globals.css";
 import { StateProvider } from "@/app/hooks/StateProvider";
+import { auth } from "@/auth";
 import { Box, CssVarsProvider } from "@mui/joy";
 import type { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,6 +15,11 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+  if (!session || !session.user) {
+    redirect('/login')
+  }
+
   return (
     <CssVarsProvider>
       <Box sx={{ height: '100vh' }}>
