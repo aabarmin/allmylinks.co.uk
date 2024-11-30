4b11e41a17318ade96d2ec38f7c7cab8ef6165d2
Aleksandr Barmin <abarmin@outlook.com>
2024-11-30 13:23:13 +0000

    Start a old good Java project

---
+++ src/test/java/dev/abarmin/aml/AllMyLinksApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abarmin.aml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllMyLinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
