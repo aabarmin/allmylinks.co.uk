f1a67c90b2f4b24bebd8e486eefe1539899d9e21
Aleksandr Barmin <abarmin@outlook.com>
2025-05-12 20:53:09 +0100

    Add header

--- aml-frontend-private/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      This is the main page of a new app
-    </>
-  )
-}
-
-export default App
--- aml-frontend-private/src/main.tsx
+++ aml-frontend-private/src/main.tsx
@@ -1,9 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router";
+import DashboardLayout from './DashboardLayout';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const router = createHashRouter([
+  {
+    path: "/",
+    Component: DashboardLayout,
+    children: []
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
